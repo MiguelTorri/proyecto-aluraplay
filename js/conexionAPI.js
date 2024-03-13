2b3008a4af63b067dd4473fb15f07f35e91d1ab4
Miguel Torrico <mickimaick18@gmail.com>
2024-03-13 16:39:22 -0400

    requisicion POST

--- js/conexionAPI.js
+++ js/conexionAPI.js
@@ -9,8 +9,24 @@ async function listarVideos() {
 
 }
 
+async function crearVideo() {
+    const conexion = await fetch ("http://localhost:3001/videos",{
+        method:"POST",
+        headers:{"content-type":"aplication/json"},
+        body:JSON.stringify({
+            titulo:titulo,
+            descripcion:descripcion,
+            url:url,
+            imagen:imagen
+        })
+    });
+    const conexionConvertida = conexion.json();
+
+    return conexionConvertida;
+}
+
 export const conexionAPI = {
-    listarVideos
+    listarVideos, crearVideo
 }
 
 //listarVideos()
