5ac373a74c863352b5db884e7cf542dbca70e22a
Miguel Torrico <mickimaick18@gmail.com>
2024-03-13 15:38:41 -0400

    terminando con forEach

--- js/mostrarVideos.js
+++ js/mostrarVideos.js
@@ -2,18 +2,18 @@ import { conexionAPI } from "./conexionAPI.js";
 
 const lista = document.querySelector("[data-lista]")
 
-function crearCard() {
+function crearCard(titulo,descripcion,url,imagen) {
     const video = document.createElement("li");
     video.className = "videos__item"
     video.innerHTML = `<li class="videos__item">
-        <iframe width="100%" height="72%" src="https://www.youtube.com/embed/QjOWz9avkg8"
-        title="Front-end vs. Back-end: ¡Descubre el lado perfecto para ti!" frameborder="0"
+        <iframe width="100%" height="72%" src="${url}"
+        title="${titulo}" frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowfullscreen></iframe>
         <div class="descripcion-video">
-        <img src="./img/logo.png" alt="logo canal alura">
-        <h3>Front-end vs. Back-end: ¡Descubre el lado perfecto para ti!</h3>
-        <p>236 mil visualizaciones</p>
+        <img src="${imagen}" alt="logo canal alura">
+        <h3>${titulo}</h3>
+        <p>${descripcion}</p>
         </div>
         </li>`;
 
@@ -21,5 +21,9 @@ function crearCard() {
 }
 
 async function listarVideos() {
-    const listaAPI = conexionAPI.listarVideos()
+    const listaAPI = await conexionAPI.listarVideos()
+
+    listaAPI.forEach(video => lista.appendChild(crearCard(video.titulo , video.descripcion , video.url , video.imagen)));
 }
+
+listarVideos()
