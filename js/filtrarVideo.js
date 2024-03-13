10d85d49cb966b33f6774c343c0cec52737f7d9b
Miguel Torrico <mickimaick18@gmail.com>
2024-03-13 19:32:30 -0400

    reutilizar funciones y arreglando  db.json

--- js/filtrarVideo.js
+++ js/filtrarVideo.js
@@ -1,12 +1,17 @@
 import { conexionAPI } from "./conexionAPI.js";
+import crearCard from "./mostrarVideos.js";
 
 async function filtrarVideo(evento){
 
     evento.preventDefault();
     const datosDeBusqueda = document.querySelector("[data-busqueda]");
-    const busqueda = conexionAPI.buscarVideos(datosDeBusqueda);
+    const busqueda = await conexionAPI.buscarVideos(datosDeBusqueda);
 
-    console.log (busqueda)
+
+    const lista = document.querySelector("[data-lista]");
+
+    busqueda.forEach(video => lista.appendChild(crearCard(video.titulo, video.descripcion, video.url, video.imagen)));
+    //console.log (busqueda)
 
 }
 
