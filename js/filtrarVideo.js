6358940706e102b67a9e4aa896e82b8182701eee
Miguel Torrico <mickimaick18@gmail.com>
2024-03-14 01:23:55 -0400

    lista filtrada

--- js/filtrarVideo.js
+++ js/filtrarVideo.js
@@ -4,11 +4,15 @@ import crearCard from "./mostrarVideos.js";
 async function filtrarVideo(evento){
 
     evento.preventDefault();
-    const datosDeBusqueda = document.querySelector("[data-busqueda]");
+    const datosDeBusqueda = document.querySelector("[data-busqueda]").value;
     const busqueda = await conexionAPI.buscarVideos(datosDeBusqueda);
 
 
     const lista = document.querySelector("[data-lista]");
+
+    while(lista.firstChild){
+        lista.removeChild(lista.firstChild);
+    }
 
     busqueda.forEach(video => lista.appendChild(crearCard(video.titulo, video.descripcion, video.url, video.imagen)));
     //console.log (busqueda)
